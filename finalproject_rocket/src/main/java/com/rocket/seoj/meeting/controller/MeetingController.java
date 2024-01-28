7a9728241efd178479b59a756adeb4e2abbd6a37
P-LEG\Administrator <seoj.dev@gmail.com>
2024-01-28 19:15:46 +0900

    변경사항 적용중

--- finalproject_rocket/src/main/java/com/rocket/seoj/meeting/controller/MeetingController.java
+++ finalproject_rocket/src/main/java/com/rocket/seoj/meeting/controller/MeetingController.java
@@ -1,4 +1,4 @@
-package com.rocket.seoj.meeting;
+package com.rocket.seoj.meeting.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MeetingController {
 
     @GetMapping("/meeting")
-    public String zoom_showForm() {
+    public String meetingPage() {
         return "meeting/meeting"; // JSP 파일 이름 (zoomForm.jsp)
     }
 
